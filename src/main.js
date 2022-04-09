cb2d0e2d83911f59406455245e25e5ca712c8594
kasiasolinska <catherine.bus@gmail.com>
2022-04-09 15:04:52 +0200

    prettiersr.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com';
+const apiUrl = "https://jsonplaceholder.typicode.com";
 
-const postsUrl = apiUrl + '/posts';
+const postsUrl = apiUrl + "/posts";
 const commentsUrl = `${apiUrl}/comments`;
 const usersUrl = `${apiUrl}/users`;
 let authorsUrl;
@@ -14,19 +14,19 @@ async function getApiResponse(url) {
 async function setAuthor(authorId) {
   const userUrl = `${usersUrl}/${authorId}`;
   const user = await getApiResponse(`${usersUrl}/${authorId}`);
-  const userElement = document.getElementById('author');
-  userElement.classList.add('author');
+  const userElement = document.getElementById("author");
+  userElement.classList.add("author");
   userElement.innerHTML = `<h3>${user.name} <small>(${user.email})</small></h3>`;
 }
 
 async function loadComments(postId) {
   const postCommentsUrl = `${commentsUrl}?postId=${postId}`;
   const comments = await getApiResponse(`${commentsUrl}?postId=${postId}`);
-  const commentsContainer = document.getElementById('comments');
-  commentsContainer.innerHTML = '';
+  const commentsContainer = document.getElementById("comments");
+  commentsContainer.innerHTML = "";
   for (const comment of comments) {
-    const commentElement = document.createElement('div');
-    commentElement.classList.add('comment');
+    const commentElement = document.createElement("div");
+    commentElement.classList.add("comment");
     commentElement.innerHTML = `
       <h4><i>${comment.name}</i> by <code>${comment.email}</code></h4>
       <p>${comment.body}</p>
@@ -36,37 +36,37 @@ async function loadComments(postId) {
 }
 
 async function addListElement(post) {
-  const element = document.createElement('li');
+  const element = document.createElement("li");
   const label = `${post.id} ${post.title}`;
   element.innerText = `${post.id} ${post.title}`;
-  element.classList.add('title');
-  element.addEventListener('click', async () => {
-    const contentElement = document.getElementById('content');
+  element.classList.add("title");
+  element.addEventListener("click", async () => {
+    const contentElement = document.getElementById("content");
     contentElement.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
     setAuthor(post.userId);
     loadComments(post.id);
   });
-  const listContainer = document.getElementById('list');
+  const listContainer = document.getElementById("list");
   listContainer.append(element);
 }
 
-document.addEventListener('DOMContentLoaded', event => {
-  const content = document.querySelector('#content');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const content = document.querySelector("#content");
 
   setTimeout(() => {
     getApiResponse(postsUrl)
-      .then(posts => {
-        content.innerHTML = 'Select post&hellip;';
+      .then((posts) => {
+        content.innerHTML = "Select post&hellip;";
 
         for (const post of posts) {
           addListElement(post);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         loader.remove();
       })
       .finally(() => {
-        const loader = document.querySelector('#spinner');
+        const loader = document.querySelector("#spinner");
         loader.remove();
       });
   }, 2000);
