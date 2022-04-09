7e1e06498e3c6f8e4d9f6ab90d3be304737695b0
kasiasolinska <catherine.bus@gmail.com>
2022-04-09 13:10:44 +0200

    style.scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  entry: ['./src/main.js', './src/style.css'],
+  entry: ['./src/main.js', './src/style.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -10,8 +10,8 @@ module.exports = {
 module: {
   rules:[
 {
-  test: /.css$/,
-  use: [MiniCSSExtractPlugin.loader,'css-loader'],
+  test: /.scss$/,
+  use: [MiniCSSExtractPlugin.loader,'css-loader', 'sass-loader'],
 },
   ],
 },
