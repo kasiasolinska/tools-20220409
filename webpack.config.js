83a85128937bc1a201f86d0f8321c35ba61e1bf3
kasiasolinska <catherine.bus@gmail.com>
2022-04-10 09:47:17 +0200

    naprawa błędów main.js)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const ESLintWebpackPlugin = require ('eslint-webpack-plugin');
+
 module.exports = {
   entry: ['./src/main.js', './src/style.scss'],
   output: {
@@ -20,6 +22,7 @@ plugins:[
   template: './src/index.html',
 }),
 new MiniCSSExtractPlugin(),
+new ESLintWebpackPlugin()
 ],
 };
 
